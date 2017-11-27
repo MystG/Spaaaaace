696721295bd8b1a985c90153cdaa1e8b425ce7d0
Liliana Campuzano <MystG19@gmail.com>
2017-11-27 02:34:22 -0500

    added poem

---
+++ spacePoem.rb
@@ -0,0 +1,212 @@
+puts " Below lies the observable universe... 
+	...for the most part
+  Like the universe, it has voids and spaces
+  and it is HUGE.
+
+  It is also dark, like the night sky I live under.
+  To me, the universe is just words I believe in.
+
+
+  "
+
+puts "<space>
+	<universe>
+		<Laniakea-Supercluster>
+			<Virgo-Supercluster>
+				<Local-Galactic-Group>
+					<Milky-Way-Galaxy>
+						<Solar-Interstellar-Neighborhood>
+							<Sun>
+								<Solar-System>
+									<Mercury>		</Mercury>
+									<Venus>		</Venus>
+									<Earth>
+										<United-States>
+											<New-York>
+												<Troy>
+													<RPI>
+														<Sage-Laboratory>
+															<Vast-Labs>
+																<Station23>		</Station23>
+																<Other-Stations>		</Other-Stations>
+															</Vast-Labs>
+															<Other-Rooms>		</Other-Rooms>
+														</Sage-Laboratory>
+														<Other-Buildings>		</Other-Buildings>
+													</RPI>
+												</Troy>
+												<New-York-City>		</New-York-City>
+												<Other-Places-In-NYS>		</Other-Places-In-NYS>
+											<Other-States>		</Other-States>
+										</United-States>
+										<Other-Countries>		</Other-Countries>
+									</Earth>
+									<Mars>		</Mars>
+									<Asteroid-Belt>		</Asteroid-Belt>
+									<Ceres>		</Ceres>
+									<Jupiter>		</Jupiter>
+									<Saturn>		</Saturn>
+									<Uranus>		</Uranus>
+									<Neptune>		</Neptune>
+									<Pluto>		</Pluto>
+									<Haumea>		</Haumea>
+									<Makemake>		</Makemake>
+									<Kuiper-Belt-and-Scattered-Disk>		</Kuiper-Belt-and-Scattered-Disk>
+									<Eris>		</Eris>
+								</Solar-System>
+							</Sun>
+							<Gamma-Cephei>		</Gamma-Cephei>
+							<Alderamin>		</Alderamin>
+							<Upsilon-Andromedae>		</Upsilon-Andromedae>
+							<HD-154354>		</HD-154354>
+							<Capella>		</Capella>
+							<Aldebaran>		</Aldebaran>
+							<Castor>		</Castor>
+							<47-Ursae-Majoris>		</47-Ursae-Majoris>
+							<Teegarden-s-Star>		</Teegarden-s-Star>
+							<55-Cancri>		</55-Cancri>
+							<Pollux>		</Pollux>
+							<Gliese-176>		</Gliese-176>
+							<51-Pegasi>		</51-Pegasi>
+							<Gliese-777>		</Gliese-777>
+							<Gliese-436>		</Gliese-436>
+							<Arcturus>		</Arcturus>
+							<Luyten-s-Star>		</Luyten-s-Star>
+							<Vega>		</Vega>
+							<Procyon>		</Procyon>
+							<Epsilon-Eridani>		</Epsilon-Eridani>
+							<Zosma>		</Zosma>
+							<HD-217107>		</HD-217107>
+							<Altair>		</Altair>
+							<Gliese-785>		</Gliese-785>
+							<Rasalhague>		</Rasalhague>
+							<Tau-Ceti>		</Tau-Ceti>
+							<Wolf-359>		</Wolf-359>
+							<Denebola>		</Denebola>
+							<83-Leonis>		</83-Leonis>
+							<HD-69830>		</HD-69830>
+							<Gliese-317>		</Gliese-317>
+							<Gliese-876>		</Gliese-876>
+							<Sirius>		</Sirius>
+							<Alpha-Centauri>		</Alpha-Centauri>
+							<Ross-128>		</Ross-128>
+							<Barnard-s-Sun>		</Barnard-s-Sun>
+							<Gliese-581>		</Gliese-581>
+							<Formalhaut>		</Formalhaut>
+							<61-Virginis>		</61-Virginis>
+							<Epsilon-Indi>		</Epsilon-Indi>
+							<HD-40307>		</HD-40307>
+							<HD-10647>		</HD-10647>
+							<Beta-Pictoris>		</Beta-Pictoris>
+							<Epsilon-Reticuli>		</Epsilon-Reticuli>
+							<Mu-Arae>		</Mu-Arae>
+							<HD-113538>		</HD-113538>
+							<Tau-Centuari>		</Tau-Centuari>
+					</Milky-Way-Galaxy>
+					<Sextans-B>		</Sextans-B>
+					<Sextans-A>		</Sextans-A>
+					<Leo-A>		</Leo-A>
+					<NGC-3109>		</NGC-3109>
+					<Antila-Dwarf>		</Antila-Dwarf>
+					<Leo-I>		</Leo-I>
+					<Leo-II>		</Leo-II>
+					<Canes-Dwarf>		</Canes-Dwarf>
+					<Ursa-Major-I>		</Ursa-Major-I>
+					<Sextans-Dwarf>		</Sextans-Dwarf>
+					<Bootes-Dwarf>		</Bootes-Dwarf>
+					<Ursa-Major-II>		</Ursa-Major-II>
+					<Ursa-Minor-Dwarf>		</Ursa-Minor-Dwarf>
+					<Draco-Dwarf>		</Draco-Dwarf>
+					<Large-Magellanic-Cloud>		</Large-Magellanic-Cloud>
+					<Small-Magellanic-Cloud>		</Small-Magellanic-Cloud>
+					<Sculptor-Dwarf>		</Sculptor-Dwarf>
+					<Fornax-Dwarf>		</Fornax-Dwarf>
+					<Sagittarius-Dwarf>		</Sagittarius-Dwarf>
+					<NGC-185>		</NGC-185>
+					<IC-10>		</IC-10>
+					<NGC-147>		</NGC-147>
+					<Andromeda-Galaxy>
+						<M31>		</M31>
+					</Andromeda-Galaxy>
+					<M110>		</M110>
+					<M32>		</M32>
+					<Andromeda-I>		</Andromeda-I>
+					<Andromeda-II>		</Andromeda-II>
+					<Andromeda-III>		</Andromeda-III>
+					<NGC-6822>		</NGC-6822>
+					<Triangulum-Galaxy>
+						<M33>		</M33>
+					</Triangulumalaxy>
+					<Pisces-Dwarf>		</Pisces-Dwarf>
+					<Phoenix-Dwarf>		</Phoenix-Dwarf>
+					<IC-1613>		</IC-1613>
+					<Aquarius-Dwarf>		</Aquarius-Dwarf>
+					<SagDIG>		</SagDIG>
+					<Pegasus-Dwarf>		</Pegasus-Dwarf>
+					<Cetus-Dwarf>		</Cetus-Dwarf>
+					<Tucana-Dwarf>		</Tucana-Dwarf>
+					<WLM>		</WLM>
+				</Local-Galactic-Group>
+				<Virgo-III-Groups>		</Virgo-III-Groups>
+				<NGC-4697>		</NGC-4697>
+				<NGC-5033>		</NGC-5033>
+				<Virgo-Cluster>		</Virgo-Cluster>
+				<Ursa-Major-Groups>		</Ursa-Major-Groups>
+				<Leo-II-Groups>		</Leo-II-Groups>
+				<Canes-Groups>		</Canes-Groups>
+				<Leo-I>		</Leo-I>
+				<NGC-2997>		</NGC-2997>
+				<NGC-5128>		</NGC-5128>
+				<M101>		</M101>
+				<M81>		</M81>
+				<Maffei>		</Maffei>
+				<NGC-6744>		</NGC-6744>
+				<Sculptor>		</Sculptor>
+				<NGC-1023>		</NGC-1023>
+				<Dorado>		</Dorado>
+				<NGC-7582>		</NGC-7582>
+				<Fornax-Cluster>		</Fornax-Cluster>
+				<Eridanus-Cluster>		</Eridanus-Cluster>
+			</Virgo-Supercluster>
+			<Hydra-Centaurus-Supercluster>		</Hydra-Centaurus-Supercluster>
+			<Pavo-Indus-Supercluster>		</Pavo-Indus-Supercluster>
+			<Southern-Supercluster>		</Southern-Supercluster>
+			<Saraswati-Supercluster>		</Saraswati-Supercluster>
+		</Laniakea-Supercluster>
+		<Nearby-Superclusters>
+			<Perseus-Pisces-Supercluster>		</Perseus-Pisces-Supercluster>
+			<Coma-Supercluster>		</Coma-Supercluster>
+			<Sculptor-Superclusters>		</Sculptor-Superclusters>
+			<Hercules-Superclusters>		</Hercules-Superclusters>
+			<Leo-Supercluster>		</Leo-Supercluster>
+			<Ophiuchus-Supercluster>		</Ophiuchus-Supercluster>
+			<Shapley-Supercluster>		</Shapley-Supercluster>
+		</Nearby-Superclusters>
+		<Distant-Superclusters>
+			<Pisces-Cetus-Supercluster>		</Pisces-Cetus-Supercluster>
+			<Bootes-Supercluster>		</Bootes-Supercluster>
+			<Horogium-Supercluster>		</Horogium-Supercluster>
+			<Corona-Borealis-Supercluster>	</Corona-Borealis-Supercluster>
+			<Columbia-Supercluster>		</Columbia-Supercluster>
+			<Aquarius-Supercluster>		</Aquarius-Supercluster>
+			<Aquarius-B-Supercluster>		</Aquarius-B-Supercluster>
+			<Aquarius-Capricornus-Supercluster>		</Aquarius-Capricornus-Supercluster>
+			<Aqaurius-Cetus-Supercluster>		</Aquarius-Cetus-Supercluster>
+			<Bootes-A-Supercluster>		</Bootes-A-Supercluster>
+			<Caelum-Supercluster>		</Caelum-Supercluster>
+			<Draco-Supercluster>		</Draco-Supercluster>
+			<Draco-Ursa-Major-Supercluster>		</Draco-Ursa-Major-Supercluster>
+			<Formax-Eridanus-Supercluster>		</Formax-Eridanus-Supercluster>
+			<Grus-Supercluster>		</Grus-Supercluster>
+			<Leo-A-Supercluster>		</Leo-A-Supercluster>
+			<Leo-Sextans-Supercluster>		</Leo-Sextans-Supercluster>
+			<Leo-Virgo-Supercluster>		</Leo-Virgo-Supercluster>
+			<Microscopium-Supercluster>		</Microscopium-Supercluster>
+			<Pegasus-Pisces-Supercluster>		</Pegasus-Pisces-Supercluster>
+			<Perseus-Pisces-Supercluster>		</Perseus-Pisces-Supercluster>
+			<Pisces-Aries-Supercluster>		</Pisces-Aries-Supercluster>
+			<Ursa-Majoris-Supercluster>		</Ursa-Majoris-Supercluster>
+			<Virgo-Coma-Supercluster>		</Virgo-Coma-Supercluster>
+		</Distant-Superclusters>
+	</universe>
+<space>"
